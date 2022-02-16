6ab19b529fb1e8ca12f6090d435c994d504a90bd
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-15 18:45:01 -0700

    Changed navbar css

---
+++ components/Subcategories.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Photography.module.css'
+
+const Subcategories = () => {
+  return (
+    <div className={styles.subcategories}>
+        <h2 className={styles.subTitles}>PEOPLE</h2>
+        <h2 className={styles.subTitles}>PEOPLE</h2>
+        <h2 className={styles.subTitles}>PEOPLE</h2>
+    </div>
+  )
+}
+
+export default Subcategories
--- pages/photography.js
+++ pages/photography.js
@@ -1,9 +1,13 @@
 import FullNav from '../components/FullNav'
+import Subcategories from '../components/Subcategories'
+
+import styles from '../styles/Photography.module.css'
 
 export default function Photography({ }) {
   return (
     <>
       <FullNav />
+      <Subcategories />
     </>
   )
 }
