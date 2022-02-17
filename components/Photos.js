5e64f4b4251e8147394395bfe61844e2dbc3185b
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-16 23:48:53 -0700

    Added photo grid

--- components/Photos.js
+++ components/Photos.js
@@ -1,9 +1,22 @@
+import Image from 'next/image'
+
+// * Photo imports
+
 import styles from '../styles/Photography.module.css'
 
 const Photos = () => {
   return (
-    <div className={styles.photoContainer}>
+    <div className={styles.overflowContainer}>
+        <div className={styles.photoContainer}>
+            {/* Column 1 */}
+            <div className={`${styles.square} ${styles.col1}`}></div>
+            {/* Column 2 */}
+            <div className={`${styles.square} ${styles.col2}`}></div>
+            <div className={`${styles.square} ${styles.col2}`}></div>
 
+            {/* Column 3 */}
+            <div className={`${styles.square} ${styles.col3} ${styles.row1}`}></div>
+        </div>
     </div>
   )
 }
