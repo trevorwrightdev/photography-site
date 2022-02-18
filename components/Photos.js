ced108f5cfac6d7ed8e6150141b9de221a4c4199
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-18 12:03:31 -0700

    Testing out swiping

--- components/Photos.js
+++ components/Photos.js
@@ -5,39 +5,38 @@ import WrappedPhoto from './WrappedPhoto'
 
 import styles from '../styles/Photography.module.css'
 
-const Photos = ({animRight}) => {
+const Photos = ({animRight, swiped}) => {
   return (
     <div className={styles.overflowContainer}>
-        <motion.div animate={{x: animRight}} transition={{type: 'spring', stiffness: 40}} className={styles.photoContainer}>
-            {/* Column 1 */}
-            <div className={styles.column}>
-              <WrappedPhoto src='/photos/janae1.jpg' height='562.5px' width='450px'/>
-              <WrappedPhoto src='/photos/savannah1.jpg' height='300.58px' width='450px'/>
-              <WrappedPhoto src='/photos/dog1.jpg' height='360px' width='450px'/>
-              <WrappedPhoto src='/photos/blueGirl.jpg' height='674px' width='450px'/>
-              <WrappedPhoto src='/photos/harper1.jpg' height='300.5px' width='450px'/>
-              <WrappedPhoto src='/photos/savannahBook.jpg' height='562.5px' width='450px'/>
-              <WrappedPhoto src='/photos/savannahGlasses.jpg' height='673.7px' width='450px'/>
-            </div>
-            {/* Column 2 */}
-            <div className={styles.column}>
-              <WrappedPhoto src='/photos/needle.jpg' height='300.75px' width='450px' />
-              <WrappedPhoto src='/photos/lake.jpg' height='562.5px' width='450px' />
-              <WrappedPhoto src='/photos/rock.jpg' height='300.56px' width='450px' />
-              <WrappedPhoto src='/photos/TimesSquareFinal.jpg' height='300.62px' width='450px' />
-              <WrappedPhoto src='/photos/salt.jpg' height='562.4px' width='450px' />
-              <WrappedPhoto src='/photos/canyon.jpg' height='300.57px' width='450px' />
-              <WrappedPhoto src='/photos/steelwool.jpg' height='300px' width='450px' />
-              <WrappedPhoto src='/photos/frozen.jpg' height='300.45px' width='450px' />
-              <WrappedPhoto src='/photos/flip.jpg' height='300.57px' width='450px' />
-            </div>
+      <motion.div animate={{x: animRight}} transition={{type: 'spring', stiffness: 40}} className={styles.photoContainer} style={swiped ? {background: red} : {}}>
+        {/* Column 1 */}
+        <div className={styles.column}>
+          <WrappedPhoto src='/photos/janae1.jpg' height='562.5px' width='450px'/>
+          <WrappedPhoto src='/photos/savannah1.jpg' height='300.58px' width='450px'/>
+          <WrappedPhoto src='/photos/dog1.jpg' height='360px' width='450px'/>
+          <WrappedPhoto src='/photos/blueGirl.jpg' height='674px' width='450px'/>
+          <WrappedPhoto src='/photos/harper1.jpg' height='300.5px' width='450px'/>
+          <WrappedPhoto src='/photos/savannahBook.jpg' height='562.5px' width='450px'/>
+          <WrappedPhoto src='/photos/savannahGlasses.jpg' height='673.7px' width='450px'/>
+        </div>
+        {/* Column 2 */}
+        <div className={styles.column}>
+          <WrappedPhoto src='/photos/needle.jpg' height='300.75px' width='450px' />
+          <WrappedPhoto src='/photos/lake.jpg' height='562.5px' width='450px' />
+          <WrappedPhoto src='/photos/rock.jpg' height='300.56px' width='450px' />
+          <WrappedPhoto src='/photos/TimesSquareFinal.jpg' height='300.62px' width='450px' />
+          <WrappedPhoto src='/photos/salt.jpg' height='562.4px' width='450px' />
+          <WrappedPhoto src='/photos/canyon.jpg' height='300.57px' width='450px' />
+          <WrappedPhoto src='/photos/steelwool.jpg' height='300px' width='450px' />
+          <WrappedPhoto src='/photos/frozen.jpg' height='300.45px' width='450px' />
+          <WrappedPhoto src='/photos/flip.jpg' height='300.57px' width='450px' />
+        </div>
 
-            {/* Column 3 */}
-            <div className={styles.column}>
-              <WrappedPhoto src='/photos/needle.jpg' height='300.75px' width='450px' />
-            </div>
-
-        </motion.div>
+        {/* Column 3 */}
+        <div className={styles.column}>
+          <WrappedPhoto src='/photos/needle.jpg' height='300.75px' width='450px' />
+        </div>
+      </motion.div>
     </div>
   )
 }
--- pages/photography.js
+++ pages/photography.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import FullNav from '../components/FullNav'
 import Subcategories from '../components/Subcategories'
 import Photos from '../components/Photos'
+import ReactTouchEvents from 'react-touch-events'
 
 import styles from '../styles/Photography.module.css'
 
@@ -9,10 +10,19 @@ export default function Photography({ }) {
 
   const [state, setState] = useState(0)
   const [animRight, setRightAnim] = useState('0')
+  const [swiped, setSwiped] = useState(false)
 
   const changeColumn = (num, distance) => {
     setState(num)
     setRightAnim(distance)
+  }
+
+  const swipe = (direction) => {
+    if (direction === 'right') {
+      setSwiped(true)
+    } else if (direction === 'left') {
+      setSwiped(false)
+    }
   }
 
   return (
@@ -20,7 +30,9 @@ export default function Photography({ }) {
       <FullNav />
       <Subcategories state={state} click0={() => changeColumn(0, '0')} click1={() => changeColumn(1, '-33.33333%')} click2={() => changeColumn(2, '-66.66666%')}/>
       <div className={styles.invisNav}></div>
-      <Photos animRight={animRight}/>
+      <ReactTouchEvents onSwipe={swipe}>
+        <Photos swiped={swiped} animRight={animRight}/>
+      </ReactTouchEvents>
     </>
   )
 }
