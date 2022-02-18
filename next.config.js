0e862177f03c28e6d47deaef0d5c5fa9720e2e6b
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-18 00:13:49 -0700

    edited webpack conifg

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.module.rules.push(
+      {test: /\.(png|jpeg)$/, loader: 'url-loader?limit=8192'},
+    )
+
+    return config
+  },
 }
