2a2d58559a98e72d56b8644dbc1ab4449c8589d6
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-17 17:37:28 -0700

    Fixed photo grid by changing to double flexbox

--- components/WrappedPhoto.js
+++ components/WrappedPhoto.js
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 
-const WrappedPhoto = ({col, row, src, height, width}) => {
+const WrappedPhoto = ({src, height, width}) => {
   return (
-    <div style={{position: 'relative', height: height, width: width, gridColumn: col, gridRow: row}}>
-        <Image src={src} layout='fill'/>
+    <div style={{position: 'relative', height: height, width: width}}>
+        <Image src={src} layout='fill' quality={100}/>
     </div>
   )
 }
