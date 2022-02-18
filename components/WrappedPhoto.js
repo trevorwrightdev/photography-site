e745518d096a63f2d7b0c49925bb355ca4557fed
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-18 10:30:52 -0700

    Changed photo quality to 75

--- components/WrappedPhoto.js
+++ components/WrappedPhoto.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const WrappedPhoto = ({src, height, width}) => {
   return (
     <div style={{position: 'relative', height: height, width: width, background: '#121212'}}>
-        <Image src={src} layout='fill' quality={100} priority/>
+        <Image src={src} layout='fill' quality={75} priority/>
     </div>
   )
 }
