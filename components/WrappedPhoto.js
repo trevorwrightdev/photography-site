4a09296aca91d8e1ff032b0a0672fbb9e8384c0f
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-18 00:34:39 -0700

    Added background load image

--- components/WrappedPhoto.js
+++ components/WrappedPhoto.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const WrappedPhoto = ({src, height, width}) => {
   return (
-    <div style={{position: 'relative', height: height, width: width}}>
+    <div style={{position: 'relative', height: height, width: width, background: '#121212'}}>
         <Image src={src} layout='fill' quality={100}/>
     </div>
   )
