d1ff1a1def97ee37d4c66dd24b19ae7043e4f8e1
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-18 00:26:39 -0700

    Tried some new photo imports

--- components/Photos.js
+++ components/Photos.js
@@ -28,30 +28,30 @@ const Photos = ({animRight}) => {
         <motion.div animate={{x: animRight}} transition={{type: 'spring', stiffness: 40}} className={styles.photoContainer}>
             {/* Column 1 */}
             <div className={styles.column}>
-              <WrappedPhoto src='../public/photos/janae1.jpg' height='562.5px' width='450px'/>
-              {/* <WrappedPhoto src={savannah1} height='300.58px' width='450px'/>
-              <WrappedPhoto src={dog1} height='360px' width='450px'/>
-              <WrappedPhoto src={ellie} height='674px' width='450px'/>
-              <WrappedPhoto src={harper1} height='300.5px' width='450px'/>
-              <WrappedPhoto src={savannahBook} height='562.5px' width='450px'/>
-              <WrappedPhoto src={savannahGlasses} height='673.7px' width='450px'/> */}
+              <WrappedPhoto src='/photos/janae1.jpg' height='562.5px' width='450px'/>
+              <WrappedPhoto src='/photos/savannah1.jpg' height='300.58px' width='450px'/>
+              <WrappedPhoto src='/photos/dog1.jpg' height='360px' width='450px'/>
+              <WrappedPhoto src='/photos/blueGirl.jpg' height='674px' width='450px'/>
+              <WrappedPhoto src='/photos/harper1.jpg' height='300.5px' width='450px'/>
+              <WrappedPhoto src='/photos/savannahBook.jpg' height='562.5px' width='450px'/>
+              <WrappedPhoto src='/photos/savannahGlasses.jpg' height='673.7px' width='450px'/>
             </div>
             {/* Column 2 */}
             <div className={styles.column}>
-              {/* <WrappedPhoto src={needle} height='300.75px' width='450px' />
-              <WrappedPhoto src={lake} height='562.5px' width='450px' />
-              <WrappedPhoto src={rock} height='300.56px' width='450px' />
-              <WrappedPhoto src={timesSquare} height='300.62px' width='450px' />
-              <WrappedPhoto src={salt} height='562.4px' width='450px' />
-              <WrappedPhoto src={canyon} height='300.57px' width='450px' />
-              <WrappedPhoto src={steelWool} height='300px' width='450px' />
-              <WrappedPhoto src={frozen} height='300.45px' width='450px' />
-              <WrappedPhoto src={flip} height='300.57px' width='450px' /> */}
+              <WrappedPhoto src='/photos/needle.jpg' height='300.75px' width='450px' />
+              <WrappedPhoto src='/photos/lake.jpg' height='562.5px' width='450px' />
+              <WrappedPhoto src='/photos/rock.jpg' height='300.56px' width='450px' />
+              <WrappedPhoto src='/photos/TimesSquareFinal.jpg' height='300.62px' width='450px' />
+              <WrappedPhoto src='/photos/salt.jpg' height='562.4px' width='450px' />
+              <WrappedPhoto src='/photos/canyon.jpg' height='300.57px' width='450px' />
+              <WrappedPhoto src='/photos/steelwool.jpg' height='300px' width='450px' />
+              <WrappedPhoto src='/photos/frozen.jpg' height='300.45px' width='450px' />
+              <WrappedPhoto src='/photos/flip.jpg' height='300.57px' width='450px' />
             </div>
 
             {/* Column 3 */}
             <div className={styles.column}>
-              {/* <WrappedPhoto src={needle} height='300.75px' width='450px' /> */}
+              <WrappedPhoto src='/photos/needle.jpg' height='300.75px' width='450px' />
             </div>
 
         </motion.div>
