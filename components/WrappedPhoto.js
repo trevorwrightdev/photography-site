d047316293fef858f6748292b6412844a78b706d
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-28 21:44:02 -0700

    Made it look better on bigger screens

--- components/WrappedPhoto.js
+++ components/WrappedPhoto.js
@@ -2,10 +2,8 @@ import Image from 'next/image'
 
 const WrappedPhoto = ({src, ratio}) => {
 
-
-
   return (
-    <div style={{position: 'relative', height: `calc(${ratio} * 90vw)`, width: '90vw', background: '#121212'}}>
+    <div style={{position: 'relative', height: `calc(${ratio} * 90vw)`, width: '90vw', maxWidth: '700px', maxHeight: `calc(${ratio} * 700px)`, background: '#121212'}}>
         <Image src={src} layout='fill' quality={75} priority/>
     </div>
   )
