49b2c901998219224716b078404242931526ef25
trevorwrightdev <trevor.wright.710@gmail.com>
2022-03-05 12:54:41 -0700

    prevented scrolling when mobile menu is open

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 
 import ScreenContext from '../components/ScreenContext'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import '../styles/globals.css'
 
@@ -8,11 +8,18 @@ function MyApp({ Component, pageProps }) {
 
   const [locked, setLocked] = useState(false)
 
+  useEffect(() => {
+    if (locked) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'visible'
+    }
+    
+  }, [locked])
+
   return (
     <ScreenContext.Provider value={[locked, setLocked]}>
-      <div style={locked ? {overflowY: 'hidden'} : {}}>
-        <Component {...pageProps} />
-      </div>
+      <Component {...pageProps} />
     </ScreenContext.Provider>
   )
 }
