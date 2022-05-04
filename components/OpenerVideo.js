599e97cd0c332db61e0995d918bf2ec0e9163d60
trevorwrightdev <trevor.wright.710@gmail.com>
2022-05-03 19:10:14 -0500

    added opener video

--- components/OpenerVideo.js
+++ components/OpenerVideo.js
@@ -1,12 +1,10 @@
 import styles from '../styles/Home.module.css'
 
-// TODO: Make this work
-
 const OpenerVideo = () => {
 
   return (
     <div className={styles.videoContainer}>
-      
+      <video style={{height: '100%', width: '100%', objectFit: 'cover'}} src='./web-vid.mp4' muted loop autoPlay></video>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Meta from '../components/Meta'
 import OpenerVideo from '../components/OpenerVideo'
-import FullNav from '../components/FullNav'
-
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
