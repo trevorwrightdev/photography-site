bbba26626a87e7670587791a5223eb5fed617460
trevorwrightdev <trevor.wright.710@gmail.com>
2022-05-04 21:28:05 -0500

    added bounciness to dorpdown

--- components/FullNav.js
+++ components/FullNav.js
@@ -6,17 +6,14 @@ import ScreenContext from './ScreenContext'
 const FullNav = () => {
 
     const [locked, setLocked] = useContext(ScreenContext)
-    const [height, setHeight] = useState(0)
     const [menuIsOpen, setMenuOpen] = useState(false)
 
     const menuOpen = () => {
-        setHeight('170px')
         setLocked(true)
         setMenuOpen(true)        
     }
 
     const menuClose = () => {
-        setHeight(0)
         setMenuOpen(false)
         setLocked(false)
     }
@@ -25,7 +22,7 @@ const FullNav = () => {
     <>
         <div className='navParent'>
             <NavBar closeMenu={menuIsOpen ? menuClose : null} clickEvent={menuIsOpen ? menuClose : menuOpen}/>
-            <MobileNav closeMenu={menuClose} menuIsOpen={menuIsOpen} heightAnim={height}/>
+            <MobileNav closeMenu={menuClose} menuIsOpen={menuIsOpen}/>
         </div>
         <div className='navInvisible'></div>
     </>
--- components/MobileNav.js
+++ components/MobileNav.js
@@ -1,10 +1,33 @@
 import Link from 'next/link'
 import { motion } from 'framer-motion'
 
-const MobileNav = ({ heightAnim, menuIsOpen, closeMenu }) => {
+const MobileNav = ({menuIsOpen, closeMenu }) => {
+
+    const variant = {
+        hidden: {
+            height: '0px',
+            width: '0px',
+            marginLeft: '180px',
+            transition: {
+                type: 'spring',
+                stiffness: 100
+            }
+        },
+        visible: {
+            height: '170px',
+            width: '180px',
+            marginLeft: '0px',
+            transition: {
+                type: 'spring',
+                stiffness: 200,
+                mass: 0.3,
+            }
+        }
+    }
+
 
     return (
-    <motion.div className='mobileNav' animate={{height: heightAnim}} transition={{duration: 0.25}}>
+    <motion.div className='mobileNav' animate={menuIsOpen ? 'visible' : 'hidden'} variants={variant}>
         <div className='mobileNavWords' style={menuIsOpen ? {display: 'flex'} : {display: 'none'}}>
             <Link href='/photography'><h1 onClick={closeMenu}>PHOTOGRAPHY</h1></Link>
             <Link href='/videography'><h1 onClick={closeMenu}>VIDEOGRAPHY</h1></Link>
