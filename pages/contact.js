5d73968380bac8e2338a9dc0f9839a6844700a62
trevorwrightdev <trevor.wright.710@gmail.com>
2022-05-04 20:13:06 -0500

    added black bottom bar

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,13 @@
 import styles from '../styles/Contact.module.css'
 
+const email = '18lmartinos@gmail.com'
+
 export default function Contact({ }) {
   return (
     <>
       <div className={styles.container}>
         <h1>CONTACT</h1>
-        <form className={styles.form} action="https://formsubmit.co/trevor.wright.710@gmail.com" method="POST">
+        <form className={styles.form} action={`https://formsubmit.co/${email}`} method="POST">
           <p>NAME</p>
           <input autoComplete='off' className={styles.name} type='text' name='name'></input>
           <p>EMAIL</p>
@@ -16,7 +18,7 @@ export default function Contact({ }) {
           </textarea>
           <button type='submit'>SEND</button>
         </form>
-        <p className={styles.emailContact}>or email:<br /><a href='mailto:john@example.com'>your@email.com</a></p>
+        <p className={styles.emailContact}>or email:<br /><a href={`mailto:${email}`}>your@email.com</a></p>
       </div>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
     <>
       <Meta title='Martinos'/>
       <OpenerVideo />
+      <div className={styles.bottomBar}/>
     </>
   )
 }
