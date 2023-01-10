3f5d1e152eb9d2f0c70721f4e4da13006c18e6b3
trevorwrightdev <trevor.wright.710@gmail.com>
2023-01-10 14:02:38 -0800

    removed scroll disable when menu is open

--- pages/_app.js
+++ pages/_app.js
@@ -12,11 +12,11 @@ function MyApp({ Component, pageProps }) {
   // ! Technically context doesn't need to be used for this, but it was good practice.
 
   useEffect(() => {
-    if (locked) {
-      document.body.style.overflow = 'hidden'
-    } else {
-      document.body.style.overflow = 'visible'
-    }
+    // if (locked) {
+    //   document.body.style.overflow = 'hidden'
+    // } else {
+    //   document.body.style.overflow = 'visible'
+    // }
     
   }, [locked])
 
