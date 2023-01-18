aca271b86850542760944ab0675ebc497f4c3604
trevorwrightdev <trevor.wright.710@gmail.com>
2023-01-18 14:35:07 -0700

    added socials

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,8 @@
 import styles from '../styles/Contact.module.css'
+import Image from 'next/image' 
+import insta from '../public/images/instagram-logo.png'
+import youtube from '../public/images/youtube-logo.png'
+import linkedin from '../public/images/linkedin-logo.png'
 
 const email = '18lmartinos@gmail.com'
 
@@ -19,6 +23,20 @@ export default function Contact({ }) {
           <button type='submit'>SEND</button>
         </form>
         <p className={styles.emailContact}>or email:<br /><a href={`mailto:${email}`}>{email}</a></p>
+        <div className={styles.socialsContainer}>
+          <div className={styles.social}>
+            <Image src={insta} height={20} width={20}/>
+            <a href="https://www.instagram.com/lee_martinos/" target="_blank" rel="noopener noreferrer">instagram</a>
+          </div>
+          <div className={styles.social}>
+            <Image src={youtube} height={20} width={20}/>
+            <a href="https://www.youtube.com/leemartinos" target="_blank" rel="noopener noreferrer">youtube</a>
+          </div>
+          <div className={styles.social}>
+            <Image src={linkedin} height={20} width={20}/>
+            <a href="https://www.linkedin.com/in/leemartinos/" target="_blank" rel="noopener noreferrer">linkedin</a>
+          </div>
+        </div>
       </div>
     </>
   )
