562262cbc8c1524315d18f07f85aac6394687ede
trevorwrightdev <trevor.wright.710@gmail.com>
2023-02-27 23:05:35 -0700

    stuff

--- components/TwinePoetry.js
+++ components/TwinePoetry.js
@@ -7,7 +7,7 @@ const TwinePoetry = () => {
           <h1>Twine Poetry</h1>
           <h3>Feb 27, 2023</h3>
       </div>
-      <iframe src='./twine.html' style={{ border: 'none', width: '100%', height: '300px' }} className={styles.iframe}/>
+      <iframe src='./twine.html' style={{ border: 'none', width: '100%', height: '900px' }} className={styles.iframe}/>
       <p className={styles.description}>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
