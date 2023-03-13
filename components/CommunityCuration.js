322b9d2f90475af5225dab1d7b2f37f0f60b4479
trevorwrightdev <trevor.wright.710@gmail.com>
2023-03-13 10:07:04 -0600

    figured something out

--- components/CommunityCuration.js
+++ components/CommunityCuration.js
@@ -7,7 +7,6 @@ import Amanda2 from '../public/community-curation/Amanda/Amanda 2.jpg'
 import Amanda3 from '../public/community-curation/Amanda/Amanda 3.jpg'
 import Amanda4 from '../public/community-curation/Amanda/Amanda 4.jpg'
 
-
 const CommunityCuration = () => {
   return (
     <>
@@ -19,14 +18,18 @@ const CommunityCuration = () => {
         <div className={styles.row}>
           <div className={styles.column}>
             <h1>Adam</h1>
-            <Image src={Adam} height={600} />
+            <div className={styles.imageContainer}>
+              <Image src={Adam} fill style={{ objectFit: 'contain' }} />
+            </div>
             <p className={styles.description}>
               My wife edited this photo of me, it turned out pretty cool 👌
             </p>
           </div>
           <div className={styles.column}>
             <h1>Ailed</h1>
-            <Image src={Ailed} width={750}/>
+            <div className={styles.imageContainer}>
+              <Image src={Ailed} fill style={{ objectFit: 'contain' }} />
+            </div>
             <p className={styles.description}>
               It is important to me because it was the first picture I took with
               a camera that wasn’t my phone.
@@ -35,17 +38,28 @@ const CommunityCuration = () => {
           <div className={styles.column}>
             <h1>Amanda</h1>
             <div className={styles.imageRow}>
-              <Image src={Amanda1} width={180}/>
-              <Image src={Amanda2} width={180}/>
-              <Image src={Amanda3} width={180}/>
-              <Image src={Amanda4} width={180}/>
+              <div className={styles.imageContainer}>
+                <Image src={Amanda1} fill style={{ objectFit: 'contain' }} />
+              </div>
+              <div className={styles.imageContainer}>
+                <Image src={Amanda2} fill style={{ objectFit: 'contain' }} />
+              </div>
+              <div className={styles.imageContainer}>
+                <Image src={Amanda3} fill style={{ objectFit: 'contain' }} />
+              </div>
+              <div className={styles.imageContainer}>
+                <Image src={Amanda4} fill style={{ objectFit: 'contain' }} />
+              </div>
             </div>
             <p className={styles.description}>
-              This was my art project in 2020. I painted my backyard walls for my kids. It’s special because I
-never thought in a million years I could actually create something so cool for my kids (I promise
-I’m not an artist that can draw). This painting represents my daughter Rose (it’s baby Moana). I
-also painted the garden wall as Bikini Bottom for Charlie. It’s definitely an art project I’m proud
-of, not only because I didn’t think I could do it but because of what it represents my family ♥️
+              This was my art project in 2020. I painted my backyard walls for
+              my kids. It’s special because I never thought in a million years I
+              could actually create something so cool for my kids (I promise I’m
+              not an artist that can draw). This painting represents my daughter
+              Rose (it’s baby Moana). I also painted the garden wall as Bikini
+              Bottom for Charlie. It’s definitely an art project I’m proud of,
+              not only because I didn’t think I could do it but because of what
+              it represents my family ♥️
             </p>
           </div>
         </div>
