1d8bea2f6eb692f657284fc30220c96e795621e7
trevorwrightdev <trevor.wright.710@gmail.com>
2023-03-13 11:31:16 -0600

    things

--- components/CommunityCuration.js
+++ components/CommunityCuration.js
@@ -31,6 +31,14 @@ import Mckay2 from '../public/community-curation/Mckay/McKay Mannequin Store-2.p
 import Mckay3 from '../public/community-curation/Mckay/McKay Mannequin Store-3.png'
 import Nathan1 from '../public/community-curation/Nathan/Nathan 1.jpeg'
 import Nathan2 from '../public/community-curation/Nathan/Nathan 2.jpeg'
+import Nathaniel from '../public/community-curation/Nathaniel/Nathaniel Baby Yoda Loves Trash Pop.jpeg'
+import Noah1 from '../public/community-curation/Noah/Noah 1.png'
+import Noah2 from '../public/community-curation/Noah/Noah 2.png'
+import Noah3 from '../public/community-curation/Noah/Noah 3.png'
+import Sam from '../public/community-curation/Sam/Sam.jpg'
+import Truman from '../public/community-curation/Truman/Truman.png'
+import Tyler from '../public/community-curation/Tyler/Tyler Wyrmroost.jpg'
+import Weston from '../public/community-curation/Weston/Weston.jpeg'
 
 const CommunityCuration = () => {
   return (
@@ -412,7 +420,7 @@ const CommunityCuration = () => {
           </div>
           <div className={styles.column}>
             <h1>Nathan</h1>
-            <div className={styles.imageRow}>
+            <div className={styles.imageRow} style={{ overflowY: 'scroll' }}>
               <div className={styles.humanContainer}>
                 <Image
                   src={Nathan1}
@@ -440,6 +448,160 @@ const CommunityCuration = () => {
               whenever I meet new people. It reminds me how great it is to
               connect with others and how important it is to be kind to any
               stranger.
+            </p>
+          </div>
+          <div className={styles.column}>
+            <h1>Nathaniel</h1>
+            <div className={styles.imageContainer}>
+              <Image
+                src={Nathaniel}
+                fill
+                style={{ objectFit: 'contain' }}
+                alt=''
+              />
+            </div>
+            <p className={styles.description}>
+              I finished this recently in Procreate. I love Baby Yoda, and I
+              love Trash Pop, so I wanted to Baby Yoda to love Trash Pop too.
+              The result is this beautiful visual representation of the phrase.
+            </p>
+          </div>
+          <div className={styles.column}>
+            <h1>Noah</h1>
+            <div className={styles.imageRow}>
+              <div className={styles.imageContainer}>
+                <Image
+                  src={Noah1}
+                  fill
+                  style={{ objectFit: 'contain' }}
+                  alt=''
+                />
+              </div>
+              <div className={styles.imageContainer}>
+                <Image
+                  src={Noah2}
+                  fill
+                  style={{ objectFit: 'contain' }}
+                  alt=''
+                />
+              </div>
+              <div className={styles.imageContainer}>
+                <Image
+                  src={Noah3}
+                  fill
+                  style={{ objectFit: 'contain' }}
+                  alt=''
+                />
+              </div>
+            </div>
+            <p className={styles.description}>
+              I’m not an artist with paints and brushes but I consider baking to
+              be an art and it’s a relief to me when things turn out like this
+              loaf did. I consider it art because it’s a personal extension of
+              myself that someone can enjoy and I can take pride in.
+              Additionally, I can always improve on making it better and more
+              tasty than the last batch! and it’s also significant as an outlet
+              for stress or desire to improve and usually requires some amount
+              of skill
+            </p>
+          </div>
+          <div className={styles.column}>
+            <h1>Sam</h1>
+            <div className={styles.imageContainer}>
+              <Image src={Sam} fill style={{ objectFit: 'contain' }} alt='' />
+            </div>
+            <p className={styles.description}>
+              This piece was my first attempt to express how ADHD feels to me. I
+              think the most poignant piece of it for me is that the face looks
+              calm—it might be three faces smashed into one, or three faces
+              pulled out of one, but it’s calm. The person experiencing it is
+              just a regular person in the midst of a storm, being pulled to a
+              million different directions, different thoughts leading off to
+              who knows where, but the person is still just there and quiet and
+              normal. I was diagnosed with ADHD only a few months ago, and it’s
+              been a cool journey learning to recognize what parts of myself and
+              ADHD and what parts are actually me. This piece has helped me
+              understand myself better because of that.
+            </p>
+          </div>
+          <div className={styles.column}>
+            <h1>Truman</h1>
+            <div className={styles.imageContainer}>
+              <Image
+                src={Truman}
+                fill
+                style={{ objectFit: 'contain' }}
+                alt=''
+              />
+            </div>
+            <p className={styles.description}>
+              So this is my aquarium that I designed recently. This is
+              significant to me because I feel a sense of joy and peace in
+              creating beautiful aquascapes. This type of art is intriguing to
+              me because I can see how design elements such as rocks, caves, and
+              plants change the behavior of fish in regards to territory
+              disputes among one another.
+            </p>
+          </div>
+          <div className={styles.column}>
+            <h1>Tyler</h1>
+            <div className={styles.imageContainer}>
+              <Image src={Tyler} fill style={{ objectFit: 'contain' }} alt='' />
+            </div>
+            <p className={styles.description} style={{ fontSize: '12px' }}>
+              The following is a detailed map I made of the Wyrmroost dragon
+              sanctuary from the Fablehaven/Dragonwatch series by Brandon Mull.
+              I made this version during quarantine in mid-2020 before my
+              mission and following the release of my individual favorite book
+              in the series, Dragonwatch: Master of the Phantom Isle. Leading up
+              to the release of the book I started rereading the series over and
+              over, having a new and renewed interest and adult perspective on
+              the story up to this point and this book not only delivered on the
+              hype, but gave me the final details needed to complete this map.
+              Upon submitting this to the author's Facebook fan page, it was
+              directly seen and commented on by Mull expressing his intrigue-
+              high praise! In Christmas of 2022, after returning from my mission
+              and finally reading the long awaited tenth and final volume,
+              Dragonwatch: Return of the Dragon Slayers, I received a package
+              that had been forwarded from my old address while I was serving in
+              Idaho printed with the cover photo for book ten. I opened it to
+              find exclusive stickers, posters, a Tshirt, knickknacks, and a
+              signed copy of the book with a note from Brandon Mull saying he
+              admired my work and appreciated my dedication to the story. The
+              coolest part was that there was a short part in the final book
+              where the characters returned to Wyrmroost and travel to a new
+              location, all the while describing their surroundings with some of
+              the same extrapolated details I included in my map, down to the
+              relative positions in the same cardinal directions as I took
+              creative liberties with to draw out. Without being officially
+              credited in the foreword, I have no doubts whatsoever that this
+              project was directly used in the official release of the final
+              book, and that blows my mind! This success has in turn renewed my
+              desire to learn and grow my skills to reflect my passion and help
+              create interesting and immersive visuals for myself and others,
+              allowing my ideas to take shape even if only in this small way so
+              far. I have hundreds of more sketches and pages drawn and planned
+              for this series and beyond that will fill much of my free time for
+              years to come.
+            </p>
+          </div>
+          <div className={styles.column}>
+            <h1>Weston</h1>
+            <div className={styles.imageContainer}>
+              <Image
+                src={Weston}
+                fill
+                style={{ objectFit: 'contain' }}
+                alt=''
+              />
+            </div>
+            <p className={styles.description}>
+              I recently finished this puzzle with my friends. It took a lot of
+              time and cooperation to put it all together. We had to coordinate
+              on which parts we were working on and when we could work on it.
+              Even though we have a physical canvas to show for it, I also
+              consider the combined time and effort to be part of the artistic
+              experience.
             </p>
           </div>
         </div>
