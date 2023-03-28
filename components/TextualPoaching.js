7a5312eb4d64184af9106fe39465f9ca9f7577d9
trevorwrightdev <trevor.wright.710@gmail.com>
2023-03-28 11:03:39 -0600

    done?

---
+++ components/TextualPoaching.js
@@ -0,0 +1,206 @@
+import styles from '../styles/CommunityCuration.module.css'
+
+const TextualPoaching = () => {
+  return (
+    <>
+      <div className={styles.topContainer}>
+        <h1>Textual Poaching</h1>
+        <p>A Debunking of Hoosier Myths</p>
+        <h3>Mar 28, 2023</h3>
+      </div>
+      <div className={styles.carousel}>
+        <div className={styles.row}>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching2.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching3.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching4.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching5.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching6.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching7.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching8.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching9.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching10.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching11.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching12.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching13.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching14.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching15.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching16.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching17.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching18.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching19.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching20.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+          <div className={styles.column}>
+            <img
+              src='./textual-poaching/Textual Poaching21.jpg'
+              alt=''
+              style={{ height: '400px' }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className={styles.bottomDescription}>
+        I struggled during this assignment to find aspects of life as a Hoosier
+        being depicted before I was born. Though I did come across this website
+        that listed off Hoosier stereotypes that everyone from Indiana should
+        accept. Here is the link: <br /> <br />
+        <a href='https://www.onlyinyourstate.com/indiana/totally-true-stereotypes-hoosiers/'>
+          https://www.onlyinyourstate.com/indiana/totally-true-stereotypes-hoosiers/
+        </a>
+        <br /> <br />
+        I found that these stereotypes did not fit me, except for a few. Thus I
+        set off to debunk some Hoosier stereotypes from my perspective. I did
+        this by taking some archived footage either from online sources relating
+        to Indiana, or from photos of myself, showing both the myth, and the
+        truth from my respective. Everything with a red background is a
+        stereotype and everything with a green background my take on that
+        stereotype. Only a few are true but about 80% of them are false. This
+        shows that the lack of representation and how I don’t feel a part of the
+        Indiana community. Not that I am necessarily against it but that I don’t
+        share the same interest as the community I grew up in. This is quite
+        opposite from the #blackgirlmagic movement which united those people
+        together. I am taking a stand against my home state. Some may believe
+        that this is too extreme or that I am being too negative to where I grew
+        up. Don’t get me wrong, I appreciate everything that Indiana offered me,
+        I just would like to currently move on to bigger and better things now.
+        I created many of the aspects of my life in Indiana in photoshop,
+        combining different elements to give it a personal touch while still
+        showing bits and pieces of Indiana. This project made me think more
+        about who I am and why I think that way. Does every young person feel
+        the need to rebel in this way because they believe they are entitled to
+        more or are they just trying to get away from their parents and make
+        their own name for themselves? Taking a deeper look into this aspect of
+        my life and realizing the rebellious nature I have inside me helps me to
+        understand my methods of thinking now as I am in college. I try to think
+        outside the box, not do what everyone else is doing, and be creative in
+        this way. Sometimes this helps my college career and sometimes it
+        hinders. But it is what makes me, me. In the end, my textual poaching
+        helping to delve deeper into who I am as a person, which community I
+        more identify with, and how to approach the future with this deeper
+        understanding of myself.
+      </div>
+    </>
+  )
+}
+
+export default TextualPoaching
--- pages/TMA295.js
+++ pages/TMA295.js
@@ -3,12 +3,15 @@ import MusicMosaic from '../components/MusicMosaic'
 import GifProject from '../components/GifProject'
 import TwinePoetry from '../components/TwinePoetry'
 import CommunityCuration from '../components/CommunityCuration'
+import TextualPoaching from '../components/TextualPoaching'
 
 export default function TMA295() {
 
   return (
     <>
       <div className={styles.container}>
+        <TextualPoaching />
+        <div className={styles.separator}/>
         <CommunityCuration />
         <div className={styles.separator}/>
         <TwinePoetry />
